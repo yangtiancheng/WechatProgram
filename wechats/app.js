371137ef656e91dc4c4753d32a2cf9e720807674
yangtiancheng <tc@yangtianchengdeMacBook-Pro.local>
2018-08-26 19:44:34 +0800

    [ADD]Tompson的第一课 - 富贵命测试#task:20180826

---
+++ wechats/app.js
@@ -0,0 +1,22 @@
+//app.js
+App({
+  onLaunch: function () {
+    
+  },
+  onShow: function(){
+   
+  },
+  onHide: function(){
+
+  },
+  onError: function(){
+
+  },
+  onPageNotFound: function(){
+
+  },
+  globalData: {
+    userInfo: null,
+    gamegrade: 0
+  }
+})
